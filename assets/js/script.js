fb9021e233a8f9313469da90ef429749a1a7820b
omarfareed64 <amorafareed10@gmail.com>
2024-01-07 00:03:37 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/**
+ * navbar variables
+ */
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+  });
+
+}
+
+
+
+/**
+ * header sticky
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});
+
+
+
+/**
+ * go top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 500 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
+  
+
+})
+const rateButton = document.querySelector(".imdb-rate-button");
+const starIcons = rateButton.querySelectorAll(".star-icon");
+
+starIcons.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    starIcons.forEach((star, i) => {
+      star.classList.toggle("rated", i <= index);
+    });
+
+    // Handle rating submission (replace with your logic)
+    console.log("Rating submitted:", index + 1);
+  });
+});
+
+;
